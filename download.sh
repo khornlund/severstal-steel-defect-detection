94ada4210b0bc215c930927a860b9dff91633ad4
Karl Hornlund <karlhornlund@gmail.com>
2019-10-19 19:11:10 +1030

    add setup script to download/unzip data

---
+++ download.sh
@@ -0,0 +1,23 @@
+apt-get install unzip -y
+apt-get isnstall wget -y
+
+mkdir ~/.kaggle
+cd ~/.kaggle
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1sEEIMjVV1yMy41tZpPFP7xKhQedY6VKm' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/1n/p')&id=1sEEIMjVV1yMy41tZpPFP7xKhQedY6VKm" -O kaggle.json && rm -rf /tmp/cookies.txt
+
+cd ~/bb/severstal-steel-defect-detection
+mkdir data
+mkdir data/raw
+mkdir data/raw/severstal-steel-defect-detection
+cd data/raw/severstal-steel-defect-detection
+kaggle competitions download -c severstal-steel-defect-detection
+unzip severstal-steel-defect-detection.zip
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=18xML9Kh0xqXzHgYR1ThRJRU_ZMzTnurT' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/1n/p')&id=18xML9Kh0xqXzHgYR1ThRJRU_ZMzTnurT" -O pseudo.csv && rm -rf /tmp/cookies.txt
+
+mkdir joined_images
+mv train_images.zip joined_images/
+mv test_images.zip joined_images/
+cd joined_images
+unzip train_images.zip
+unzip test_images.zip
